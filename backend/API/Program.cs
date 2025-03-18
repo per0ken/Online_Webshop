2d9d7101481456c8a60ab0dfa4d1a2060122ccd6
per0ken <peter.lukacs16@gmail.com>
2025-03-18 20:23:33 +0100

    Filling SQLite db with dummy data, productcontroller

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -1,23 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddDbContext<StoreContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
+
+DbInitializer.InitDb(app);
 
 app.Run();
