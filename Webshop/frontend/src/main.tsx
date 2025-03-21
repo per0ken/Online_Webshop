c1436dd80fa09644d328ab5af1f10e2efde3e399
per0ken <peter.lukacs16@gmail.com>
2025-03-21 14:58:34 +0100

    Cleanup

--- Webshop/frontend/src/main.tsx
+++ Webshop/frontend/src/main.tsx
@@ -13,7 +13,7 @@ import { store } from './app/store/store';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} future={{ v7_startTransition: true }} />
+      <RouterProvider router={router} />
     </Provider>
 
   </StrictMode>,
