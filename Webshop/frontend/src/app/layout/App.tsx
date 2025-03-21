80b08a3884c91522b709ae1aeee6121a96defef3
per0ken <peter.lukacs16@gmail.com>
2025-03-21 14:55:15 +0100

    Fixing structure, using RTK Query to fetch and catching

--- Webshop/frontend/src/app/layout/App.tsx
+++ Webshop/frontend/src/app/layout/App.tsx
@@ -1,44 +1,39 @@
-import { useState } from "react"
-import {Box, Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import { Box, Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import NavBar from "./NavBar";
 import { Outlet } from "react-router-dom";
+import { useAppSelector } from "../store/store";
+
 
 function App() {
-
-
-  const [darkMode, setDarkMode] = useState(false);
-  const paletteType = darkMode ? 'dark' : 'light'
+  const {darkMode} = useAppSelector(state => state.ui);
+  const palleteType = darkMode ? 'dark' : 'light'
   const theme = createTheme({
     palette: {
-      mode: paletteType,
+      mode: palleteType,
       background: {
-        default: (paletteType === 'light') ? '#eaeaea' : '#121212'
+        default: (palleteType === 'light') ? '#eaeaea' : '#121212'
       }
     }
-  })
-
-const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-}
-
+  });
 
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      <CssBaseline />
+      <NavBar />
       <Box
         sx={{
           minHeight: '100vh',
           background: darkMode 
-          ? 'radial-gradient(circle, #1e3aBa, #111B27)' 
-          : 'radial-gradient(circle, #baecf9, #f0f9ff)',
+            ? 'radial-gradient(circle, #1e3aBa, #111B27)'
+            : 'radial-gradient(circle, #baecf9, #f0f9ff)',
           py: 6
         }}
       >
-        <Container maxWidth='xl' sx={{mt: 8}}>
+        <Container maxWidth='xl' sx={{ mt: 8 }}>
           <Outlet />
         </Container>
       </Box>
+
     </ThemeProvider>
 
   )
