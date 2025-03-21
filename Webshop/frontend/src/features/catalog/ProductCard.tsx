d7de00dfd0f049ff990d7b058227ca51fbab2ca0
per0ken <peter.lukacs16@gmail.com>
2025-03-21 14:56:12 +0100

    Catalog fix

--- Webshop/frontend/src/features/catalog/ProductCard.tsx
+++ Webshop/frontend/src/features/catalog/ProductCard.tsx
@@ -20,7 +20,7 @@ export default function ProductCard({product}: Props) {
     >
         <CardMedia 
             sx={{height: 240, backgroundSize: 'cover'}}
-            image={product.pictureUrl}
+            image={product.imageUrl}
             title={product.name}
         />
         <CardContent>
