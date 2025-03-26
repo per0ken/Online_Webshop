7fcc2e0671cf3bcb98b2097d44635235a5ec669b
per0ken <peter.lukacs16@gmail.com>
2025-03-26 20:30:22 +0100

    Adding identity, login, register, roles

--- Webshop/API/Program.cs
+++ Webshop/API/Program.cs
@@ -1,6 +1,8 @@
-using Microsoft.EntityFrameworkCore;
 using API.Data;
+using API.Entities;
 using API.Middleware;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,17 +14,27 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 builder.Services.AddCors();
 builder.Services.AddTransient<ExceptionMiddleware>();
+builder.Services.AddIdentityApiEndpoints<User>(opt =>
+{
+    opt.User.RequireUniqueEmail = true;
+})
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<StoreContext>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(opt =>
+app.UseCors(opt => 
 {
     opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://localhost:3000");
 });
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
+app.MapGroup("api").MapIdentityApi<User>(); // api/login
 
 DbInitializer.InitDb(app);
 
